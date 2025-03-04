c477d09fd722d8239ea36daae30546c8ee01b231
time4you2025 <timo.schaefer@gmx.net>
2025-03-04 13:09:08 +0100

    Update ProductionProgress.js

--- src/components/ProductionProgress.js
+++ src/components/ProductionProgress.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Card, CardContent } from "@/components/ui/Card";
+import { Card, CardContent } from "../components/ui/Card";
 import { Checkbox } from "@/components/ui/Checkbox";
 import { Progress } from "@/components/ui/Progress";
 import { Button } from "@/components/ui/Button";
