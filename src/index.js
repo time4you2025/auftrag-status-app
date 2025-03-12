fa6829858188303223a8bc8d9611346360b3bf24
time4you2025 <timo.schaefer@gmx.net>
2025-03-12 13:05:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  // deine Konfigurationsdaten
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app(); // wenn Firebase bereits initialisiert wurde
-}
