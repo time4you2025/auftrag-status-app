ac73cc2b53d36f0981a6c752befbadd6a333aac2
time4you2025 <timo.schaefer@gmx.net>
2025-03-12 13:26:23 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,6 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Stelle sicher, dass die Umgebungsvariablen existieren
+if (!import.meta.env.VITE_FIREBASE_API_KEY) {
+  throw new Error("Firebase API Key fehlt! Bitte .env-Datei prüfen.");
+}
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
