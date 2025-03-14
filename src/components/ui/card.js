3434aeee18425ff31cdb6ffa85f7a0c5264ef9ac
time4you2025 <timo.schaefer@gmx.net>
2025-03-14 12:12:07 +0100

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export function Card({ children, className, statusColor }) {
   return (
-    <div className={`bg-white rounded-lg shadow-md p-4 ${className}`}>
+    <div className={`bg-white rounded-lg shadow-md p-4 ${className} relative`}>
       {/* Status Punkt */}
       <div
-        className={`w-3 h-3 rounded-full absolute top-2 right-2 ${statusColor}`}
+        className={`w-4 h-4 rounded-full absolute top-2 right-2 ${statusColor} z-10`}
       ></div>
       {children}
     </div>
