4bae3df24774d67e2afaa05ae38b35937ae996f5
time4you2025 <timo.schaefer@gmx.net>
2025-03-14 13:51:00 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
