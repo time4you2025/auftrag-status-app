e57e98530703ec82c3c0a86598bf42ff06f23314
time4you2025 <timo.schaefer@gmx.net>
2025-03-17 15:44:56 +0100

    Update button.js

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children, onClick, className = "" }) => {
     return (
-        <button onClick={onClick} className={`px-4 py-2 bg-blue-500 text-white rounded ${className}`}>
+        <button onClick={onClick} className={`px-3 py-1 bg-blue-500 text-white rounded text-sm h-10 ${className}`}>
             {children}
         </button>
     );
