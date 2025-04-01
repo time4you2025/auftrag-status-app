aacac1a087ee115f0c55b6e815517f29fe9ed08e
time4you2025 <timo.schaefer@gmx.net>
2025-04-01 13:25:17 +0200

    Update ProductionProgress.js

--- src/components/ProductionProgress.js
+++ src/components/ProductionProgress.js
@@ -328,7 +328,7 @@ const clearSearch = () => {
     transition-all duration-300
   `}
 />
-          <div className="flex flex-col gap-2 mt-2">
+          <div className="flex flex-col gap-8 mt-2">
             {steps.map((step, index) => (
               <label key={index} className="flex items-center gap-1 text-xs">
                 <Checkbox checked={order.progress[index]} onChange={() => toggleStep(order.id, index)} />
